0a673ec5c9884d63566f14149d18eb3f8dabc51c
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-05-08 11:42:47 +0300

    just saving

--- src/utils/download_resource.rs
+++ src/utils/download_resource.rs
@@ -9,14 +9,13 @@ use super::get_path;
 pub async fn dl_resource(name: String, id: String) -> Result<(), reqwest::Error> {
     let client = Client::new();
     let url = format!("https://api.mangadex.org/at-home/server/{}", id);
-    // let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+
     let response = client.get(url).send().await?.text().await?;
     let json: AtHomeServer = serde_json::from_str(response.as_str()).unwrap();
 
     std::fs::create_dir(get_path(format!("{name}/{id}").as_str())).unwrap();
 
     for image in json.chapter.data.iter() {
-        // println!("{}/{}", json.base_url, image)
         let url = format!("{}/{}", json.base_url, image);
         let bytes = client.get(url).send().await?.text().await?;
         let file_path = get_path(format!("{name}/{id}/{image}").as_str());
